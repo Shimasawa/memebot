d7afb10c254b44f32ffd668e44d126d7fcd90a15
Shimasawa <honnomushi@rakuten.jp>
2022-08-18 20:43:38 +0900

    不要なファイルの削除

--- module/decorater.py
+++
@@ -1,8 +0,0 @@
-from os import environ
-from discord.ext import commands
-
-ADMINID = int(environ["ADMINID"])
-PENID = int(environ["PENID"])
-
-def is_admin(ctx: commands.Context):
-    return ctx.author.id == ADMINID
